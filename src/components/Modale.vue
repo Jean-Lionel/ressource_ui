cc1d9e488df6086f8fa309d67324093ba5fc0e9a
Jean Lionel <nijeanlionel@gmail.com>
2021-07-01 06:46:15 +0200

    creating modal component

---
+++ src/components/Modale.vue
@@ -0,0 +1,65 @@
+<template>
+
+    <div class="block_modale" v-if="revele">
+        <div class="overlay"></div>
+        <div class="modal card">
+            <div class="btn_close" @click="toggleModale">X</div>
+            <div>
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "modale",
+    props: ['toggleModale', 'revele']
+    
+}
+</script>
+
+<style scoped>
+
+.block_modale{
+    position:fixed;
+    bottom:0;
+    top:0;
+    left:0;
+    right:0;
+    width:100%;
+    height:100%;
+    display: flex;
+    justify-content:center;
+    align-items:center;
+}
+
+.overlay{
+    background: rgba(0,0,0,0.1);
+    position:fixed;
+    bottom:0;
+    top:0;
+    left:0;
+    right:0;
+}
+
+.modal{
+    background: #FFFFFF;
+    color: #2c3e50;
+    position:fixed;
+    padding: 50px;
+    top: 30%;
+}
+
+.btn_close{
+    position:absolute;
+    top:5px;
+    right:5px;
+    background:red;
+    padding: 5px;
+    cursor: pointer;
+
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <modale v-bind:revele="revele" :toggleModale="toggleModale">
+            <h1>Modale</h1>
+
+            <input class="form-control" type="number" v-model="number1">
+            <input class="form-control" type="number" v-model="number2">
+            <div>
+            <h1> {{ result }} </h1>
+            
+            </div>
+
+            <div>
+            
+            </div>
+
+            <button @click="helloWorld">Enregistrer</button>
+        </modale>
+        <button @click="toggleModale">OpenModate</button>
+    </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Modale from '../components/Modale.vue'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { Modale },
+
+  data() {
+      return {
+          revele : false,
+          number1 : 0,
+          number2 : 0,
+          result : 0
+      }
+  },
+  methods: {
+      toggleModale(){
+          this.revele = !this.revele;
+      },
+      helloWorld(){
+          this.result = this.number1 *1 + this.number2 * 1
+      }
+  },
+    
 }
 </script>
+
+<style>
+
+</style>
